da386a17ce5a267008fa6349037b9de916ecd6ab
Zsolt <zsolt.nagy@vodafone.com>
2022-02-16 11:45:52 +0100

    init commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { createClient } from "contentful"
+import RecipeCard from "../components/RecipeCard"
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+
+  const response = await client.getEntries({ content_type: 'recipe' })
+
+  return {
+    props: {
+      recipes: response.items
+    }
+  }
+}
+
+export default function Recipes({ recipes }) {
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard recipe={recipe} key={recipe.sys.id} />
+      ))}
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+    </div>
+  )
+}
